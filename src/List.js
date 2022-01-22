c86933018058d729dd1db466872d8cffbd346f4a
Haoran Zhou <kens0serious@gmail.com>
2022-01-22 18:46:44 -0500

    completed

--- src/List.js
+++ src/List.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((ppl) => {
+        const { id, name, age, image } = ppl
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
